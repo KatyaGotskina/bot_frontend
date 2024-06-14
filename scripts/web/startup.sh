21b3723d96872df67b8dff19da54bb3c1451f234
katya <edgotskina@greenatom.ru>
2024-06-14 23:46:14 +0300

    added start by webhook

--- scripts/web/startup.sh
+++ scripts/web/startup.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 if [[ $WEBHOOK_URL != "" ]];
   then
-    exec uvicorn src.main:create_app --host=$BIND_IP --port=$BIND_PORT
+    exec uvicorn core.main:create_app --host=$BIND_IP --port=$BIND_PORT
   else
     exec python core/tg_bot.py
 fi;
